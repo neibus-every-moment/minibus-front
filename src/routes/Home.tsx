c9c234fd0b717da210515d518084ee4262f3cb07
kimyerimweb <kimyerimweb@gmail.com>
2022-01-21 12:22:43 +0900

    feat: Create and style Home page

---
+++ src/routes/Home.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import OrderSelector from '../components/OrderSelector';
+import TagList from '../components/TagList';
+
+function Home() {
+  const [list, setList] = useState(['지하철', '버스', '택시', '기타교통수단', '지하철', '버스', '택시', '기타교통수단']);
+
+  return (
+    <div className='container'>
+      <div className='background'>
+        <div className='row'>
+          <div className='col-sm-4'>
+            <TagList list={list}/>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='col-sm-4'>
+            <TagList list={list}/>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-4">
+            <OrderSelector />
+          </div>
+        </div>
+      </div>
+
+
+      {/* 게시글 컴포넌트 */}
+    </div>
+  );
+}
+
+export default Home;
+
