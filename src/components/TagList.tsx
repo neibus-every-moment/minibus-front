8f6223ec348d06c7a198fe930e41c68ecac7e10b
lazy-sky <1cielo@naver.com>
2022-01-22 04:08:26 +0900

    fix: Change tag key to random number

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -6,9 +6,18 @@ interface TagListProps {
   list: Array<string>;
 }
 
-function TagList({ list }:TagListProps) {
+function TagList({ list }: TagListProps) {
   return (<div className='tag'>
-    <ul className="tag_list first">{list.map(el => (<TagItem item={el} key={el}/>))}</ul>
+    <ul className="tag_list first">
+      {list.map(el => (
+        /*
+        TODO: 그냥 임시로 만든 거라 키값을 대충 두신 거 같기는 한데,
+        Home에서 에러 메시지가 너무 많이 떠서 디버깅이 번거로워 임시방편으로
+        키값을 난수로 대체했습니다.
+        */
+        <TagItem item={el} key={Math.random()}/>
+      ))}
+    </ul>
   </div>);
 }
 
