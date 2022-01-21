0b70ee98576743d3960f7041a05af5e165d213db
kimyerimweb <kimyerimweb@gmail.com>
2022-01-21 16:15:24 +0900

    design: Add class to add margin only first taglist

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -8,7 +8,7 @@ interface TagListProps {
 
 function TagList({ list }:TagListProps) {
   return (<div className='tag'>
-    <ul className="tag_list">{list.map(el => (<TagItem item={el} key={el}/>))}</ul>
+    <ul className="tag_list first">{list.map(el => (<TagItem item={el} key={el}/>))}</ul>
   </div>);
 }
 
