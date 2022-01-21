26b229aa7815631fc2c15604404ab307318f8485
kimyerimweb <kimyerimweb@gmail.com>
2022-01-21 15:59:45 +0900

    design: Add img source url and shadow

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,53 +1,64 @@
 import React, { MouseEventHandler } from 'react';
 
 interface SideMenuProps {
-  onSideMenuToggle: MouseEventHandler<HTMLButtonElement>;
+  onSideMenuToggle: MouseEventHandler;
 }
 
 function SideMenu ({ onSideMenuToggle }:SideMenuProps) {
+  const defaultPath = 'http://localhost:3000/';
+
   return (
-    <aside className='side_menu'>
-      <h2 className='sr-only'>사이드 메뉴</h2>
-      <button type="button" aria-label="닫기 버튼" onClick={onSideMenuToggle}>
-        <div className='close_btn'>
-          <img src="" alt="닫기 버튼" />
+    <>
+      <aside className='side_menu'>
+        <h2 className='sr-only'>사이드 메뉴</h2>
+        <button type="button" aria-label="닫기 버튼" onClick={onSideMenuToggle} className='close_btn'>
+          <div>
+            <img src="/static/icons/cancel-black.svg" alt="닫기 버튼" />
+          </div>
+        </button>
+        <ul className='page_link_list'>
+          <li className='page_link_item'>
+            <a href={defaultPath}>지금 어디가?</a>
+          </li>
+          <li className='page_link_item'>
+            <a href={`${defaultPath}bingo`}>도전! 일상빙고</a>
+          </li>
+          <li className='page_link_item'>
+            <a href={`${defaultPath}quiz`}>교통상식 테스트</a>
+          </li>
+          <li className='page_link_item'>
+            <a href={`${defaultPath}faq`}>FAQ</a>
+          </li>
+        </ul>
+
+        <div className='side_menu_bottom'>
+          <ul className='external_link_list'>
+            <li className='external_link_item'>
+              <a href="">
+                <img src='/static/icons/neibus.svg' alt="네이버스 바로가기 링크" />
+              </a>
+            </li>
+            <li className='external_link_item'>
+              <a href="">
+                <img src="/static/icons/instagram.svg" alt="네이버스 인스타그램 바로가기" />
+              </a>
+            </li>
+            <li className='external_link_item'>
+              <a href="">
+                <img src="/static/icons/facebook.svg" alt="네이버스 페이스북 바로가기" />
+              </a>
+            </li>
+          </ul>
+
+          <p className='copyright'>&copy; Nei&amp;Company Corp.</p>
         </div>
-      </button>
-      <ul className='page_link_list'>
-        <li className='page_link_item'>
-          <a href="">지금 어디가?</a>
-        </li>
-        <li className='page_link_item'>
-          <a href="">도전! 일상빙고</a>
-        </li>
-        <li className='page_link_item'>
-          <a href="">교통상식 테스트</a>
-        </li>
-        <li className='page_link_item'>
-          <a href="">FAQ</a>
-        </li>
-      </ul>
 
-      <ul className='external_link_list'>
-        <li className='external_link_item'>
-          <a href="">
-            <img src="" alt="" />
-          </a>
-        </li>
-        <li className='external_link_item'>
-          <a href="">
-            <img src="" alt="" />
-          </a>
-        </li>
-        <li className='external_link_item'>
-          <a href="">
-            <img src="" alt="" />
-          </a>
-        </li>
-      </ul>
 
-      <p>&copy; Nei&amp;Company Corp.</p>
-    </aside>);
+      </aside>
+
+      <div className='shadow' onClick={onSideMenuToggle}></div>
+    </>
+  );
 }
 
 export default SideMenu;
