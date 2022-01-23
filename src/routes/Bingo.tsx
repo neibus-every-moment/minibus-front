282322b650dd6b36cfa12fb4a32da42cca049817
lazy-sky <1cielo@naver.com>
2022-01-23 16:26:18 +0900

    feat: Set Bingo layout

--- src/routes/Bingo.tsx
+++ src/routes/Bingo.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Bingo() {
+  const initialBingoBoard = [...new Array(16)].map((v, i) => i + 1);
+  const [bingoBoard, setBingoBoard] = useState(initialBingoBoard);
+
+  // TODO: 나중에 서버에서 24시간마다 새 빙고판 가져오도록
+  // useEffect(() => {
+  // setBingoBoard();
+  // }, [])
+
   return (
-    <div>Bingo</div>
+    <div className="container">
+      <div className="bingo">
+        <div className="row">
+          <div className="col-sm-4 bingo-info">
+            <h4>도전! 일상 빙고</h4>
+            <p>
+                이동 중 마주친<br />
+                일상 속 소소한 순간들을<br />
+                빙고판에 남겨보세요
+            </p>
+          </div>
+          <div className="col-sm-4">
+            <div>0 빙고!</div>
+            <ul className="bingo-board">
+              {bingoBoard.map(item => (
+                <li key={item}>{item}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="col-sm-4">
+            <button className="bingo-share_btn">
+              내 빙고 공유하기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
