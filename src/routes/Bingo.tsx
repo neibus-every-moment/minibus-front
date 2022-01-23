35e154bae21d49779f562509e35d41f14d1d8c2f
lazy-sky <1cielo@naver.com>
2022-01-23 18:12:17 +0900

    design: Styling bingo board

--- src/routes/Bingo.tsx
+++ src/routes/Bingo.tsx
@@ -1,7 +1,24 @@
 import React, { useEffect, useState } from 'react';
 
 function Bingo() {
-  const initialBingoBoard = [...new Array(16)].map((v, i) => i + 1);
+  const initialBingoBoard = [
+    '오늘의 빙고 어쩌구',
+    '내일의 빙고 저쩌구',
+    '이러쿵 저러쿵',
+    '동네 강아지와 마주쳤다',
+    '오늘의 출근/등교 패션',
+    '텀블러와 함께',
+    '유선 이어폰 사용자 발견',
+    '오늘의 빙고 어쩌구',
+    '내일의 빙고 저쩌구',
+    '이러쿵 저러쿵',
+    '동네 강아지와 마주쳤다',
+    '오늘의 출근/등교 패션',
+    '텀블러와 함께',
+    '유선 이어폰 사용자 발견',
+    '화면 채우기',
+    '화면 채우기',
+  ];
   const [bingoBoard, setBingoBoard] = useState(initialBingoBoard);
   const [bingoCount, setBingoCount] = useState(0);
 
@@ -26,7 +43,11 @@ function Bingo() {
             <div className="bingo-count">{bingoCount} 빙고!</div>
             <ul className="bingo-board">
               {bingoBoard.map(item => (
-                <li key={item}>{item}</li>
+                <li key={item}>
+                  <div>
+                    {item}
+                  </div>
+                </li>
               ))}
             </ul>
           </div>
