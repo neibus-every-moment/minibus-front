9cd07eb6ca40be95b41b1f4dd315f14681d6dbff
kimyerimweb <kimyerimweb@gmail.com>
2022-01-30 00:00:04 +0900

    fix: Fix not working Saga
    기존 코드에서는 rootReducer만 store 만들 때 참고했고 고친건 saga도 동작하게 만든 configueStore함수

--- client.tsx
+++ client.tsx
@@ -4,12 +4,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
 
-import rootReducer from './src/redux/ducks';
+import configureStore from './src/redux/configueStore';
 import App from './src/routes/App';
 
-const store = createStore(rootReducer);
+const store = configureStore();
 
 render(
   <Provider store={store}>
