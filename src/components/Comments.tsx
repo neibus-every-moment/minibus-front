0a32ef04b1b39c028702a96b201c4d089da7de45
lazy-sky <1cielo@naver.com>
2022-01-29 20:16:01 +0900

    feat: Fill Comments contents(temp)

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -2,12 +2,32 @@ import React from 'react';
 
 function Comments() {
   return (
-    <ul>
-      <li>comment1</li>
-      <li>comment2</li>
-      <li>comment3</li>
-      <li>comment4</li>
-    </ul>
+    <>
+      {/* TODO: 댓글 입력 컴포넌트 */}
+      <ul>
+        <li>
+          <div>프로필 사진</div>
+          <div>닉네임</div>
+          <div>시간</div>
+          <div>댓글 내용</div>
+          <div>점 세 개 버튼</div>
+        </li>
+        <li>
+          <div>프로필 사진</div>
+          <div>닉네임</div>
+          <div>시간</div>
+          <div>댓글 내용</div>
+          <div>점 세 개 버튼</div>
+        </li>
+        <li>
+          <div>프로필 사진</div>
+          <div>닉네임</div>
+          <div>시간</div>
+          <div>댓글 내용</div>
+          <div>점 세 개 버튼</div>
+        </li>
+      </ul>
+    </>
   );
 }
 
