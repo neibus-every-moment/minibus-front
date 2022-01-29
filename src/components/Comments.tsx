602c017ef1ee3c8ce6d3c50a083e18d7ee97d4a2
lazy-sky <1cielo@naver.com>
2022-01-29 19:29:59 +0900

    feat: Make Comments

---
+++ src/components/Comments.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Comments() {
+  return (
+    <ul>
+      <li>comment1</li>
+      <li>comment2</li>
+      <li>comment3</li>
+      <li>comment4</li>
+    </ul>
+  );
+}
+
+export default Comments;
