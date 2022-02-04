ee9e3498e3a5b435fccd9f9ca1a8c52e9cdd93eb
lazy-sky <1cielo@naver.com>
2022-02-05 01:41:10 +0900

    conf: Change port number

--- webpack.config.ts
+++ webpack.config.ts
@@ -79,7 +79,7 @@ const config: Configuration = {
   },
   devServer: {
     historyApiFallback: true,
-    port: 8080,
+    port: 3000,
     devMiddleware: { publicPath: '/dist/' },
     static: { directory: path.resolve(__dirname) },
   },
