64376d67b9c24f5c43f72c579a90c2601ef86b27
kimyerimweb <kimyerimweb@gmail.com>
2022-02-05 19:02:02 +0900

    feat: Create Banner component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Banner () {
+  return (
+    <aside>
+      <h2 className="sr-only">퀴즈페이지로 이동</h2>
+      <Link to ="/quiz">
+        <img src="" alt="퀴즈페이지로 이동" />
+      </Link>
+    </aside>
+  );
+
+}
+
+export default Banner;
