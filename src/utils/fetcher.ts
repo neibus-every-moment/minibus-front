68d83124d8e7332d8953888eb0e207f9ec80b13a
lazy-sky <1cielo@naver.com>
2022-02-07 19:57:38 +0900

    feat: Make fetcherWithParams

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
-const fetcher = (url: string) => {
+export const fetcher = (url: string) => {
   return axios.get(url)
     .then((res) => res.data.data)
     .catch((e) => console.error(e));
 };
 
-export default fetcher;
+export const fetcherWithParams = (url: string, params: any) => {
+  return axios.get(url, {
+    params: { ...params },
+  })
+    .then((res) => res.data.data)
+    .catch((e) => console.error(e));
+};
