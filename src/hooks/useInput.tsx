6f4762dc6c19f63bed49c7cdcf00c857a0a05542
kimyerimweb <kimyerimweb@gmail.com>
2022-02-07 11:48:40 +0900

    feat: Change initial value to empty string

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -1,9 +1,9 @@
 import { useCallback, useState } from 'react';
 
-export default (initialValue = null) => {
+export default (initialValue = '') => {
   const [value, setValue] = useState(initialValue);
-  const handler = useCallback((e) => {
+  const onChange = useCallback((e) => {
     setValue(e.target.value);
   }, []);
-  return [value, handler];
+  return [value, onChange];
 };
