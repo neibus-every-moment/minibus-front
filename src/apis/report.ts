e67400c67072a598be9660ab441ebdcc8416e11d
kimyerimweb <kimyerimweb@gmail.com>
2022-02-08 05:57:45 +0900

    feat: Create report api and hide error handling

--- src/apis/report.ts
+++ src/apis/report.ts
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
+interface reportApiBody {
+    postId: number,
+    reportReasonId: number,
+    detail?: string
+}
+
 export async function getReportReasonsApi() {
   try {
     const { data: { data } }
@@ -7,6 +13,26 @@ export async function getReportReasonsApi() {
 
     return data;
   } catch (e) {
+    console.error(e);
+  }
+}
+
+export async function postReportApi(body:reportApiBody) {
+  try {
+    const { data } = await axios.post(
+      'http://3.37.182.59:8080/api/report',
+      body
+    );
+
+    if (data === true) {
+      alert('신고가 접수되었습니다!');
+      return;
+    }
+
+    alert('다시 시도해주세요');
+    return;
+  } catch (e) {
+    alert('다시 시도해주세요');
     console.error(e);
   }
 }
