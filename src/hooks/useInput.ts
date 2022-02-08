dbda2c155713437b9667d25b2afb9ef105c0cb68
lazy-sky <1cielo@naver.com>
2022-02-08 23:06:36 +0900

    feat: Add clear function in useInput

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -2,8 +2,11 @@ import { useCallback, useState } from 'react';
 
 export default <T>(initialValue: T) => {
   const [value, setValue] = useState(initialValue);
+
   const onChange = useCallback((e) => {
     setValue(e.target.value);
   }, []);
-  return [value, onChange] as const;
+
+  const clearInput = useCallback(() => setValue(initialValue), []);
+  return [value, onChange, clearInput] as const;
 };
