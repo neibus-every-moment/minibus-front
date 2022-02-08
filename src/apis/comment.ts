2286b0bc68f2fed4bda144fc176249d0467c856e
lazy-sky <1cielo@naver.com>
2022-02-08 22:39:31 +0900

    Make createComment api call

---
+++ src/apis/comment.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const baseUrl = 'http://3.37.182.59:8080/api';
+
+interface CommentBodyProps {
+  postId: number,
+  userId: number,
+  content: string,
+}
+
+export const createComment = async (body: CommentBodyProps) => {
+  const {
+    postId,
+    userId,
+    content,
+  } = body;
+
+  try {
+    const { data: { data } } = await axios.post(`${baseUrl}/comment`, {
+      postId,
+      userId,
+      content,
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
