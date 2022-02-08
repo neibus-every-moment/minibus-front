709954690c9bf6a1bdcf10d392ebbfdab634bd44
lazy-sky <1cielo@naver.com>
2022-02-08 21:38:51 +0900

    rename: Change CreateComment to CommentCretae

---
+++ src/components/CommentCreate.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import useInput from '../hooks/useInput';
+
+function CommentCreate() {
+  const [comment, handleChangecomment] = useInput('');
+
+  return (
+    <div>
+      {/* TODO: avatar 부분, 로그인 후 useSelector */}
+      <form action="">
+        <input
+          type="text"
+          placeholder="댓글을 입력하세요"
+          value={comment}
+          onChange={handleChangecomment}
+        />
+        <button type="submit">등록</button>
+      </form>
+    </div>
+  );
+}
+
+export default CommentCreate;
--- src/components/CommentsWrapper.tsx
+++ src/components/CommentsWrapper.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import { CommentsProps } from '../routes/Home';
+import CommentCreate from './CommentCreate';
 import CommentList from './CommentList';
 
 function CommentsWrapper({ comments }: {comments: CommentsProps}) {
   return (
     <>
-      {/* TODO: 댓글 입력 컴포넌트 */}
-      <input type="text" placeholder="댓글을 입력하세요" />
+      <CommentCreate />
       <CommentList comments={comments} />
     </>
   );
