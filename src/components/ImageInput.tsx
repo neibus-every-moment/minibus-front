50c25d7f90c6e094a51211a0afbc84f1addfb1ba
kimyerimweb <kimyerimweb@gmail.com>
2022-02-10 02:08:34 +0900

    feat: Seperate Input of file from PostCreate

---
+++ src/components/ImageInput.tsx
@@ -0,0 +1,99 @@
+import React, { SetStateAction, useCallback, useRef, useState } from 'react';
+
+interface ImagesInfo {
+  [x: string]: string;
+}
+
+interface ImageInputProps {
+    imageFiles: File[],
+    setImageFiles: React.Dispatch<SetStateAction<File[]>>
+}
+
+function ImageInput({ imageFiles, setImageFiles }:ImageInputProps) {
+  const [imagesInfo, setImagesInfo] = useState<ImagesInfo>({});
+
+  const handleUploadImages = (
+    e:React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (imageFiles.length >= 10) {
+      alert('사진은 10개 이하로 등록해주세요!');
+      return;
+    }
+
+    if (e.target.files) {
+      const newImagesInfo:ImagesInfo = {};
+      const newImageFiles = [];
+      const submitImagesCount = e.target.files.length > 10 ?
+        10 : e.target.files.length;
+      const submitableImageCount = 10 - imageFiles.length;
+      const limit = submitImagesCount > submitableImageCount ?
+        submitableImageCount : submitImagesCount;
+
+      for (let i = 0;i < limit ;i++) {
+        const newImagePath = URL.createObjectURL(e.target.files[i]);
+        if (!Object.keys(imagesInfo).includes(newImagePath)) {
+          newImagesInfo[newImagePath] = e.target.files[i].name;
+          newImageFiles.push(e.target.files[i]);
+        }
+      }
+      setImagesInfo({ ...imagesInfo, ...newImagesInfo });
+      setImageFiles([...imageFiles, ...newImageFiles]);
+    }
+  };
+
+  const handleDeleteImages
+    = useCallback((e:any) => {
+      if (Object.keys(imagesInfo).includes(e.target.id)) {
+        delete imagesInfo[e.target.id];
+        setImageFiles([
+          ...imageFiles.filter(
+            file => {
+              return file.name !== e.target.value;
+            }),
+        ]);
+      } //여기서 e:any는 e.target.id때문에 작성했습니다. 원래 Event객체에 id라는 값이 없지만 써야해서..
+    }, [imagesInfo, setImagesInfo, imageFiles, setImageFiles]);
+
+
+  const imageInputRef = useRef<any>();
+  const handleClickUploadButton = () => {
+    imageInputRef.current.focus();
+  };
+
+
+  return (
+    <>
+      <label htmlFor="images" onClick={handleClickUploadButton}>
+        <img src="../static/icons/icon-camera.svg" alt="사진 첨부" />
+      </label>
+      <input
+        type="file"
+        id="images"
+        accept="image/*"
+        multiple
+        hidden
+        ref={imageInputRef}
+        onChange={handleUploadImages}
+      />
+      <p>게시글 등록 시 파일명에 한글이 포함되면 안됩니다.</p>
+      {Object.entries(imagesInfo).map(image => (
+        <div key={image[0]} >
+          <button
+            type="button"
+            onClick={handleDeleteImages}
+            id={image[0]}
+            value={image[1]}>
+                  삭제
+          </button>
+          <img
+            src={image[0]}
+            alt="유저 등록 이미지"
+            width="100px" />
+        </div>
+      ))}
+
+    </>
+  );
+}
+
+export default ImageInput;
