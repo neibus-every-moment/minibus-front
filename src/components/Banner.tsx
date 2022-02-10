42a7449f0000825f09d55ee5a5c53a3a2760ddf6
kimyerimweb <kimyerimweb@gmail.com>
2022-02-10 20:35:04 +0900

    design: Style Banner and change sm-margin to 20px

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 function Banner () {
   return (
-    <aside>
+    <aside className="banner">
       <h2 className="sr-only">퀴즈페이지로 이동</h2>
       <Link to ="/quiz">
-        <img src="" alt="퀴즈페이지로 이동" />
+        <img src="/static/images/quizBanner.png" alt="퀴즈페이지로 이동" />
       </Link>
     </aside>
   );
