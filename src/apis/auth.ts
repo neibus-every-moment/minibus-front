e3d6b425b6b35df306561d4c08dc03e547830524
lazy-sky <1cielo@naver.com>
2022-02-11 22:19:24 +0900

    feat: Make saveAuth api call

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -2,11 +2,28 @@ import axios from 'axios';
 
 import { baseUrl } from './baseUrl';
 
+export async function saveAuth() {
+  try {
+    const resToken = await axios.post(`${baseUrl}/auth/login`, {
+      email: 'test@test.com',
+      nickname: 'test계정입니다',
+      profileImage: 'test',
+    });
+
+    const ACCESS_TOKEN = resToken.data.token;
+    localStorage.setItem('Auth', ACCESS_TOKEN);
+
+  } catch (e) {
+    console.error(e);
+  }
+}
+
 export async function getUserInfo(userId: number) {
   try {
     const { data } = await axios.get(`${baseUrl}/auth/login/${userId}`);
 
     console.log(data);
+    console.log(data.token);
     return data;
   } catch (e) {
     console.error(e);
