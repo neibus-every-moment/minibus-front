b1807ecd40327b7930670ad6b000540743fb36de
kimyerimweb <kimyerimweb@gmail.com>
2022-02-12 16:05:58 +0900

    feat: Add quiz to use doda

---
+++ src/routes/Quiz.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Quiz() {
+  return (
+    <div className="container">
+      <div className="quiz-container">
+        <div className="row">
+          <div className="col-sm-4">
+            <iframe
+              src="https://doda.app/quiz/XjpTRHA086"
+              className="quiz"
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Quiz;
